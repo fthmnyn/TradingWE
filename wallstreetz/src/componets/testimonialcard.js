f86e3e31aa33e31787216e879e86f320383634fb
Hera <Bethjason23@gmail.com>
2023-07-27 09:25:19 +0300

    update

--- wallstreetz/src/componets/testimonialcard.js
+++ wallstreetz/src/componets/testimonialcard.js
@@ -4,13 +4,13 @@ const Testimonial = () => {
     return (
         <div style={{
             WebkitFontSmoothing: 'antialiased',
-        color: '#181B31',
-        fontSize: '0.875rem',
-        fontWeight: 400,
-        lineHeight: 1.43,
-        boxSizing: 'inherit',
-        fontFamily: 'inherit',
-        display: 'block'
+            color: '#181B31',
+            fontSize: '0.875rem',
+            fontWeight: 400,
+            lineHeight: 1.43,
+            boxSizing: 'inherit',
+            fontFamily: 'inherit',
+            display: 'block'
         }}>
             <div className="testimonial" style={{
                 WebkitFontSmoothing: 'antialiased',
@@ -75,7 +75,7 @@ const Testimonial = () => {
                             minWidth: '64px',
                             boxSizing: 'border-box',
                             transition:
-                              'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+                                'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
                             fontFamily: 'Montserrat, Verdana, sans-serif',
                             lineHeight: 1.75,
                             borderRadius: '4px',
@@ -120,40 +120,28 @@ const Testimonial = () => {
                             justifyContent: 'space-between',
                             backgroundColor: '#6368C9',
                         }}>
-                        <div className="testimonial-quote-img" style={{
-                            WebkitFontSmoothing: 'antialiased',
-                            fontFamily: 'inherit',
-                            color: 'rgb(255, 255, 255)',
-                            fontSize: '1.25rem',
-                            fontWeight: 500,
-                            lineHeight: 1.2,
-                        }}>
-                            <img width="44" height="31" src="https://www.wallstreetzen.com/packs/media/images/plans-image-1-quote-5e22ca724b6ce9b8b867cdf638c1be05.png" alt="Quote Icon" />
-                        </div>
-                        <div style={{
-                            WebkitFontSmoothing: 'antialiased',
-                            color: '#fff',
-                            fontSize: '1.25rem',
-                            fontWeight: 500,
-                            lineHeight: 1.2,
-                            boxSizing: 'inherit',
-                            fontFamily: 'inherit',
-                        }}>
-                            I was a finance major many moons ago, but your analysis just makes it very easy to make quick informative decisions...good job!!
-                        </div>
-                        <div className="testimonial-info" style={{
-                             WebkitFontSmoothing: 'antialiased',
-                             color: '#fff',
-                             fontSize: '1.25rem',
-                             fontWeight: 500,
-                             lineHeight: 1.2,
-                             boxSizing: 'inherit',
-                             fontFamily: 'inherit',
-                             display: 'flex',
-                             marginTop: '32px',
-                             alignItems: 'center',
-                        }}>
-                            <div className="testimonial-info-img" style={{
+                            <div className="testimonial-quote-img" style={{
+                                WebkitFontSmoothing: 'antialiased',
+                                fontFamily: 'inherit',
+                                color: 'rgb(255, 255, 255)',
+                                fontSize: '1.25rem',
+                                fontWeight: 500,
+                                lineHeight: 1.2,
+                            }}>
+                                <img width="44" height="31" src="https://www.wallstreetzen.com/packs/media/images/plans-image-1-quote-5e22ca724b6ce9b8b867cdf638c1be05.png" alt="Quote Icon" />
+                            </div>
+                            <div style={{
+                                WebkitFontSmoothing: 'antialiased',
+                                color: '#fff',
+                                fontSize: '1.25rem',
+                                fontWeight: 500,
+                                lineHeight: 1.2,
+                                boxSizing: 'inherit',
+                                fontFamily: 'inherit',
+                            }}>
+                                I was a finance major many moons ago, but your analysis just makes it very easy to make quick informative decisions...good job!!
+                            </div>
+                            <div className="testimonial-info" style={{
                                 WebkitFontSmoothing: 'antialiased',
                                 color: '#fff',
                                 fontSize: '1.25rem',
@@ -164,9 +152,8 @@ const Testimonial = () => {
                                 display: 'flex',
                                 marginTop: '32px',
                                 alignItems: 'center',
-                                marginRight: '13px', // New style property
                             }}>
-                                <img width="44" height="44" src="https://www.wallstreetzen.com/packs/media/images/main-testimonial-1-29c05e18c81517fdc1e148d6b06173b9.png" alt="User Avatar" style={{
+                                <div className="testimonial-info-img" style={{
                                     WebkitFontSmoothing: 'antialiased',
                                     color: '#fff',
                                     fontSize: '1.25rem',
@@ -174,39 +161,52 @@ const Testimonial = () => {
                                     lineHeight: 1.2,
                                     boxSizing: 'inherit',
                                     fontFamily: 'inherit',
-                                }} />
-                            </div>
-                            <div className="testimonial-info-content" style={{
-                                WebkitFontSmoothing: 'antialiased',
-                                color: '#fff',
-                                fontSize: '1.25rem',
-                                fontWeight: 500,
-                                lineHeight: 1.2,
-                                boxSizing: 'inherit',
-                                fontFamily: 'inherit',
-                                marginRight: '-16px',
-                            }}>
-                                <div className="testimonial-info-name" style={{
+                                    display: 'flex',
+                                    marginTop: '32px',
+                                    alignItems: 'center',
+                                    marginRight: '13px', // New style property
+                                }}>
+                                    <img width="44" height="44" src="https://www.wallstreetzen.com/packs/media/images/main-testimonial-1-29c05e18c81517fdc1e148d6b06173b9.png" alt="User Avatar" style={{
+                                        WebkitFontSmoothing: 'antialiased',
+                                        color: '#fff',
+                                        fontSize: '1.25rem',
+                                        fontWeight: 500,
+                                        lineHeight: 1.2,
+                                        boxSizing: 'inherit',
+                                        fontFamily: 'inherit',
+                                    }} />
+                                </div>
+                                <div className="testimonial-info-content" style={{
                                     WebkitFontSmoothing: 'antialiased',
                                     color: '#fff',
-                                    boxSizing: 'inherit',
-                                    fontFamily: 'inherit',
-                                    fontWeight: 600,
-                                    lineHeight: 1.25,
-                                    marginBottom: '2px',
-                                    fontSize: '0.875rem',
-                                }}>Jake Wood</div>
-                                <div className="testimonial-info-subscriber" style={{
-                                    WebkitFontSmoothing: 'antialiased',
+                                    fontSize: '1.25rem',
                                     fontWeight: 500,
+                                    lineHeight: 1.2,
                                     boxSizing: 'inherit',
                                     fontFamily: 'inherit',
-                                    color: '#DDDFE3',
-                                    fontSize: '0.75rem',
-                                    lineHeight: 1.25,
-                                }}>WallStreetZen Premium Subscriber</div>
+                                    marginRight: '-16px',
+                                }}>
+                                    <div className="testimonial-info-name" style={{
+                                        WebkitFontSmoothing: 'antialiased',
+                                        color: '#fff',
+                                        boxSizing: 'inherit',
+                                        fontFamily: 'inherit',
+                                        fontWeight: 600,
+                                        lineHeight: 1.25,
+                                        marginBottom: '2px',
+                                        fontSize: '0.875rem',
+                                    }}>Jake Wood</div>
+                                    <div className="testimonial-info-subscriber" style={{
+                                        WebkitFontSmoothing: 'antialiased',
+                                        fontWeight: 500,
+                                        boxSizing: 'inherit',
+                                        fontFamily: 'inherit',
+                                        color: '#DDDFE3',
+                                        fontSize: '0.75rem',
+                                        lineHeight: 1.25,
+                                    }}>WallStreetZen Premium Subscriber</div>
+                                </div>
                             </div>
-                        </div>
                         </div>
                     </div>
                 </div>
